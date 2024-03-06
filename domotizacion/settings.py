8cdd525b5e811f5f5eadf6823ba2e1b5c094d15c
John Arley Cano Salinas <johnarleycano@hotmail.com>
2024-03-06 14:52:13 -0500

    Permitidos orígenes cruzados

--- domotizacion/settings.py
+++ domotizacion/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-)lmlk*a1_6&!zm5___8$!zq3-tt8&!_#l!1armk1@o3*wgopi1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -38,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'domotizacion_api',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = 'domotizacion.urls'
@@ -126,3 +127,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000'
+]
