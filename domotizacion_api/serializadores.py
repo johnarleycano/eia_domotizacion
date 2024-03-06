c6764d9bd5d45324293b25684f81d7c90980e77a
John Arley Cano Salinas <johnarleycano@hotmail.com>
2024-03-06 14:53:17 -0500

    otros cambios

--- domotizacion_api/serializadores.py
+++ domotizacion_api/serializadores.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 class UbicacionesSerializador(serializers.Serializer):
+    id = serializers.IntegerField()
     nombre = serializers.CharField(max_length=255)
     usuario_id = serializers.IntegerField()
 
